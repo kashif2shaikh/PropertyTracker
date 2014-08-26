f1dc9c07544af54fc0050556bf4544d7b39be720
Kashif Shaikh <kshaikh@angus-systems.com>
2014-08-26 15:44:06 -0400

    Xamarin WORKING COMMIT

---
+++ PropertyTracker.Core/ViewModels/BaseViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace PropertyTracker.Core.ViewModels
+{
+
+    public abstract class BaseViewModel : MvxViewModel
+    {
+        //
+        // Tag each view model with unique instance ID
+        // 
+        public Guid ViewInstanceId {get; set;}
+
+        protected BaseViewModel()
+        {
+           ViewInstanceId = Guid.NewGuid();
+        }
+        public bool ShowViewModel<TViewModel>() where TViewModel : IMvxViewModel
+        {
+            var requestedBy = new MvxRequestedBy();
+
+            //
+            // The Current (Parent) view model that is requesting to show a *NEW* view will have the Guid assigned into the 
+            // requestedBy additional info column.
+            //
+            // The Presenter will pass this new view to the parent that has matching Guid, so Parent can show this view.
+            //
+            requestedBy.AdditionalInfo = ViewInstanceId.ToString();
+            return ShowViewModel(
+                typeof(TViewModel),
+                null,
+                null,
+                requestedBy);
+        }
+    }
+}
