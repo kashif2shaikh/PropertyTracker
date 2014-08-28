e977179edf806b20fd1b5ba81ddf8e3e1b9b7659
Kashif Shaikh <kshaikh@angus-systems.com>
2014-08-28 19:25:36 -0400

    Base Api Commit

---
+++ PropertyTracker.Web.Api/Controllers/BaseApiController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using PropertyTracker.Web.Entity.Models;
+using PropertyTracker.Web.Api.Security;
+
+namespace PropertyTracker.Web.Api.Controllers
+{
+    public class BaseApiController : ApiController
+    {
+        public User GetLoggedInUser()
+        {
+            // User is already logged in - verify.
+            if (!HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                throw new Exception("Consistency issue - user is supposed to be authenticated");
+                //return InternalServerError(new Exception("Consistency issue - user is supposed to be authenticated"));
+            }
+
+            var userIdentity = HttpContext.Current.User.Identity as UserIdentity;
+            if (userIdentity == null || userIdentity.User == null)
+            {
+                throw new Exception("Consistency issue - user identity not found");
+            }
+            return userIdentity.User;
+        }
+    }
+}
---
+++ PropertyTracker.Web.Api/TypeMapping/PropertyEntityListToPropertyDtoListTypeMapping.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using PropertyTracker.Dto.Models;
+using PropertyTracker.Web.Api.AutoMapping;
+
+namespace PropertyTracker.Web.Api.TypeMapping
+{
+    public class PropertyEntityListToPropertyDtoListTypeMapping : IAutoMapperTypeMapping
+    {
+        public void Configure()
+        {
+            Mapper.CreateMap<IEnumerable<Entity.Models.Property>, Dto.Models.PropertyList>()
+                .ForMember(dm => dm.Properties, em => em.ResolveUsing<PropertyListResolver>())
+                ; 
+        }
+
+        protected class PropertyListResolver : ValueResolver<IEnumerable<Entity.Models.Property>, IList<Dto.Models.Property>>
+        {
+            protected override IList<Property> ResolveCore(IEnumerable<Entity.Models.Property> source)
+            {
+                return Mapper.Map<IEnumerable<Entity.Models.Property>, IList<Dto.Models.Property>>(source);
+            }
+        }
+    }
+}
