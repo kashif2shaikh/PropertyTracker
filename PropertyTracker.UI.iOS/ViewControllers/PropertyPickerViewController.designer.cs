5a6400633a81f1760d4886659c23a09e892f5240
Kashif Shaikh <kshaikh@angus-systems.com>
2014-09-14 15:27:39 -0400

    user detail view controller

--- PropertyTracker.UI.iOS/ViewControllers/PropertyPickerViewController.designer.cs
+++ PropertyTracker.UI.iOS/ViewControllers/PropertyPickerViewController.designer.cs
@@ -1,12 +1,10 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
-using System;
 using MonoTouch.Foundation;
-using MonoTouch.UIKit;
 using System.CodeDom.Compiler;
 
 namespace PropertyTracker.UI.iOS.ViewControllers
@@ -22,9 +20,23 @@ namespace PropertyTracker.UI.iOS.ViewControllers
 
 		[Outlet]
 		MonoTouch.UIKit.UIToolbar PropertyToolBar { get; set; }
-
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (GetMorePropertiesButtonItem != null) {
+				GetMorePropertiesButtonItem.Dispose ();
+				GetMorePropertiesButtonItem = null;
+			}
+
+			if (GetPropertiesButtonItem != null) {
+				GetPropertiesButtonItem.Dispose ();
+				GetPropertiesButtonItem = null;
+			}
+
+			if (PropertyToolBar != null) {
+				PropertyToolBar.Dispose ();
+				PropertyToolBar = null;
+			}
 		}
 	}
 }
--- PropertyTracker.UI.iOS/ViewControllers/UserDetailViewController.designer.cs
+++ PropertyTracker.UI.iOS/ViewControllers/UserDetailViewController.designer.cs
@@ -1,17 +1,34 @@
+// WARNING
 //
-// This file has been generated automatically by Visual Studio to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
 
 namespace PropertyTracker.UI.iOS.ViewControllers
 {
-    [Register("UserDetailViewController")]
-    partial class UserDetailViewController : BaseUserViewController
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("UserDetailViewController")]
+	partial class UserDetailViewController : BaseUserViewController
+	{
+		[Outlet]
+		MonoTouch.UIKit.UIBarButtonItem CancelButtonItem { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIBarButtonItem EditButtonItem { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (CancelButtonItem != null) {
+				CancelButtonItem.Dispose ();
+				CancelButtonItem = null;
+			}
+
+			if (EditButtonItem != null) {
+				EditButtonItem.Dispose ();
+				EditButtonItem = null;
+			}
+		}
+	}
 }
